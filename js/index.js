5a4cf5e79011cd88abd4a55f23c6b87ed3bd6d8f
sunflwoer519 <92281084+sunflower519sf@users.noreply.github.com>
2024-01-30 15:36:17 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,44 +1,103 @@
 const countdownElement = document.getElementById('countdown');
 
 
-const countToDate = new Date("Feb 10, 2024 0:0:0").getTime();
-// const countToDate = new Date("Dec 31, 2023 14:8:0").getTime();
+// const countToDate = new Date("Feb 10, 2024 0:0:0").getTime();
+const countToDate = new Date("Dec 31, 2023 14:8:0").getTime();
 var eveAudio = document.getElementById('eve-audio');
+var NYAudio = document.getElementById('NY-audio');
+var NYAudio2 = document.getElementById('NY-audio2');
+
+
+function eveVideoEnd(event) {
+    if(event.data === 0) {          
+        document.querySelectorAll('.start-eve').forEach(element => {element.style.display = 'none';});
+        document.querySelectorAll('.eve-end').forEach(element => {element.style.display = 'block';});
+        document.querySelectorAll('.eve-video').forEach(element => {element.style.display = 'none';});
+    }
+}
+
+function NYVideoEnd(event) {
+    if(event.data === 0) {          
+        document.querySelectorAll('.NY-video').forEach(element => {element.style.display = 'none';});
+        document.querySelectorAll('.NY-ask').forEach(element => {element.style.display = 'block';});
+        NYAudio2.src = "https://github.com/sunflower519sf/lunarNY2024/blob/main/audio/audio2.MP3?raw=true"
+        NYAudio2.play()
+        NYAudio2.addEventListener('ended', function() {
+            document.querySelectorAll('.NY-ask').forEach(element => {element.style.display = 'none';});
+            window.location = "ask.html"
+        });
+    }
+}
+
+// youtube影片播放控制
+var youtuPlayer;
+function onYouTubePlayerAPIReady(yotuDivID, youtuID, endFunction) {
+    youtuPlayer = new YT.Player(yotuDivID, {
+        // width: '640',
+        // height: '390',
+        videoId: youtuID,
+        playerVars: {
+            controls:0,
+            disablekb:1
+        },
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: endFunction
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// function onPlayerStateChange(event) {        
+//     if(event.data === 0) {          
+//         alert('done');
+//     }
+// }
 
 
 
 function eveVideoYes() {
     document.querySelectorAll('.start-eve').forEach(element => {element.style.display = 'none';});
     document.querySelector(".eve-video").style.display = "block";
+    onYouTubePlayerAPIReady("eve-video", "KbBOATiwank", eveVideoEnd);
 }
 
 function eveVideoNo() {
-    eveAudio.src = "https://github.com/sunflower519sf/lunarNY2024/blob/main/audio/eve-video.MP3?raw=true"
+    eveAudio.src = "https://github.com/sunflower519sf/lunarNY2024/blob/main/audio/eve-video.MP3?raw=true";
     document.querySelectorAll('.start-eve').forEach(element => {element.style.display = 'none';});
-    eveAudio.play()
+    eveAudio.play();
     eveAudio.addEventListener('ended', function() {
         document.querySelectorAll('.start-eve').forEach(element => {element.style.display = 'none';});
         document.querySelectorAll('.eve-end').forEach(element => {element.style.display = 'block';});
     });
 }
 
+
+
+
+
+
 const now = new Date().getTime();
 const distance = countToDate - now;
-if (distance > 0) {
+if (distance < 0) {
+    document.querySelectorAll('.start-NY').forEach(element => {element.style.display = 'none';});
+    document.querySelectorAll('.start-lunarNY').forEach(element => {element.style.display = 'block';});
+        
+} else {
     document.querySelectorAll('.eve-before').forEach(element => {element.style.display = 'block';});
+
 }
+
 const countdownInterval = setInterval( () => {
     const now = new Date().getTime();
     const distance = countToDate - now;
 
 
     if (distance < 0) {
-        document.querySelectorAll('.start-NY').forEach(element => {element.style.display = 'none';});
-        document.querySelectorAll('.start-lunarNY').forEach(element => {element.style.display = 'block';});
         
-
-
-
 
     } else {
         
@@ -58,7 +117,7 @@ document.getElementById('startButton').addEventListener('click', function() {
     document.querySelectorAll('.eve-before').forEach(element => {element.style.display = 'none';});
     document.querySelectorAll('.eve-after').forEach(element => {element.style.display = 'block';});
 
-    eveAudio.play()
+    eveAudio.play();
     eveAudio.addEventListener('ended', function() {
         document.querySelectorAll('.eve-after').forEach(element => {element.style.display = 'none';});
         document.querySelectorAll('.start-eve').forEach(element => {element.style.display = 'block';});
@@ -66,10 +125,17 @@ document.getElementById('startButton').addEventListener('click', function() {
 });
 
 
-
 document.getElementById('startButton-NY').addEventListener('click', function() {
-    
-    
+    document.querySelectorAll('.lunarNY-before').forEach(element => {element.style.display = 'none';});
+    document.querySelectorAll('.lunarNY-after').forEach(element => {element.style.display = 'block';});
+    NYAudio.src = "https://github.com/sunflower519sf/lunarNY2024/blob/main/audio/audio1.MP3?raw=true";
+    NYAudio.play();
+    NYAudio.addEventListener('ended', function() {
+        document.querySelectorAll('.lunarNY-after').forEach(element => {element.style.display = 'none';});
+        document.querySelectorAll('.start-lunarNY').forEach(element => {element.style.display = 'none';});
+        document.querySelectorAll('.NY-video').forEach(element => {element.style.display = 'block';});
+        onYouTubePlayerAPIReady("NY-video", "WfP-E_7Uoco", NYVideoEnd);
+    });
 
 });
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,40 @@
-
+const fireworkContainer = document.querySelector('.backGround');
 const audioButton = document.getElementById('startButton');
 const asks = document.querySelectorAll(".answer");
 
 let askCount = 0;
 let errorCheck = false
 
+function endPage() {
+    document.querySelectorAll('.pageAsk').forEach(element => {element.style.display = 'none';});
+    document.querySelectorAll('.check-jump').forEach(element => {element.style.display = 'block';});
+}
+
+// youtube影片播放控制
+var youtuPlayer;
+function onYouTubePlayerAPIReady(yotuDivID, youtuID, endFunction) {
+    youtuPlayer = new YT.Player(yotuDivID, {
+        // width: '640',
+        // height: '390',
+        videoId: youtuID,
+        playerVars: {
+            controls:0,
+            disablekb:1
+        },
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: endFunction
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
 
 function endChart() {
     
-
     let errorLable = [''];
     for(let i = 1; i < errorCount.length; i++) {
         if (!errorCount[i] == 0) {
@@ -42,7 +68,11 @@ function endChart() {
                 font: {
                     size: 50
                 }
+              },
+              legend: {
+                display: false // 禁用图例
               }
+
             },
             scales: {
               x: {
@@ -89,15 +119,28 @@ function endChart() {
                     stepSize: 1
                 }
               }
-            },
-            elements: {
-                line: {
-                    hide: false // 禁用點擊線段時隱藏的功能
-                }
             }
         }
     }
-    const myChart = new Chart(ctxChart, configChart);
+
+
+    if (errorCheck){
+        const myChart = new Chart(ctxChart, configChart);
+
+    } else {
+        document.querySelectorAll('.pageEnd').forEach(element => {element.style.display = 'none';});
+        document.querySelectorAll('.endNoError').forEach(element => {element.style.display = 'block';});
+        const fireworks = new Fireworks(fireworkContainer, {
+            speed: 4,
+            acceleration: 1.05,
+            friction: 1,
+            gravity: 4,
+            particles: 400,
+            explosion: 10
+        });
+        fireworks.start();
+    }
+    
 }
 
 
@@ -112,7 +155,7 @@ function askEnd() {
     document.querySelector(".pageAsk").style.display = "none";
     document.querySelector(".pageEnd").style.display = "block";
     document.querySelector(".backGround").style.backgroundImage = "url(../img/background-dark.jpg)";
-    endChart();
+    endPage();
 }
 
 function askError() {
@@ -147,6 +190,7 @@ document.getElementById('startButton').addEventListener('click', function() {
     document.querySelector(".pageStart").style.display = "none";
     document.querySelector(".pageAsk").style.display = "block";
 
+    
     fetch('setting.json')
     .then(response => response.json())
     .then(data => {
@@ -154,7 +198,7 @@ document.getElementById('startButton').addEventListener('click', function() {
         askReplace();
 
         if (!(window.errorCount = localStorage.getItem("errorCount"))) {
-            window.errorCount = Array(data.ask.length).fill(0);
+            window.errorCount = Array(data.ask.length+1).fill(0);
         } else {
             window.errorCount = JSON.parse(window.errorCount);
         }
@@ -181,4 +225,10 @@ asks.forEach(button => {
     });
 });
 
+document.getElementById('check-jump').addEventListener('click', function() {
+    document.querySelectorAll('.check-jump').forEach(element => {element.style.display = 'none';});
+    document.querySelectorAll('.pageStart').forEach(element => {element.style.display = 'none';});
+    endChart()
+
+});
 
